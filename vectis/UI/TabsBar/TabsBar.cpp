864a44d7183828d89e37abe0e760f9bbc0452b6b
marco.diiga@gmail.com <marco.diiga@gmail.com>
2014-11-20 16:38:13 +0100

    Completed bezièr work both sx and dx, first tab is visible!

--- vectis/UI/TabsBar/TabsBar.cpp
+++ vectis/UI/TabsBar/TabsBar.cpp
@@ -22,24 +22,62 @@ void TabsBar::paintEvent ( QPaintEvent* ) {
     opt.init(this);
     QPainter p(this);
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
-
-    // Disegna la barra orizzontale in basso che si collega al CodeTextEdit
-    QPen hp1( QColor( 60, 61, 56 ) ), hp2( QColor( 11, 11, 10 ) );
-    p.setPen(hp1);
-    //qDebug() << rect();
-    p.drawLine( rect().left(), rect().bottom(), rect().right(), rect().bottom() );
-    p.setPen(hp2);
-    p.drawLine( rect().left(), rect().bottom() - 1, rect().right(), rect().bottom() - 1 );
-
-    // test di disegno di una tab a 0;0 - 50;31
-    QPainterPath path;
-    path.moveTo(0, 31); // Mi muovo in basso a sx
-    path.cubicTo(20,31, 10,0, 30,0);
-    path.lineTo(30,31);
-    //path.lineTo(0,31);
-    p.setPen(QColor(255,255,0));
-    //p.setBrush(QColor(122, 163, 39));
     p.setRenderHint(QPainter::Antialiasing);
-    p.drawPath(path);
+
+    // La barra orizzontale che si interrompe solo sulla tab selezionata corrente, è di due pixel
+    // e ha due colori:
+    //
+    //  -- nero rgb(11,11,10)   -- bcol1
+    //  -- grigio rgb(60,61,56) -- gcol2
+    const QColor outerBlackCol(11, 11, 10);
+    const QColor innerGrayCol(60, 61, 56);
+
+    // La tab attualmente selezionata (può essere nulla)
+    QRect selectedTabRect(5, 5, 150 /* Width */, rect().bottom() - 5);
+
+    // Disegna la barra orizzontale in basso che si collega al CodeTextEdit;
+    // questa corre per tutto il code editor *tranne* che per la selected tab
+    const QPen grayPen( innerGrayCol ), blackPen( outerBlackCol );
+    p.setPen(grayPen);
+    p.drawLine( rect().left(), rect().bottom(), selectedTabRect.left(), rect().bottom());
+    p.drawLine( selectedTabRect.right(), rect().bottom(), rect().right(), rect().bottom());
+    p.setPen(blackPen);
+    p.drawLine( rect().left(), rect().bottom() - 1, selectedTabRect.left(), rect().bottom() - 1);
+    p.drawLine( selectedTabRect.right(), rect().bottom() - 1, rect().right(), rect().bottom() - 1);
+
+    p.setPen(QColor(255,0,0)); // Debug
+    //p.drawRect(tabRect); // Debug
+
+    // Disegna due curve di bezièr e una linea orizzontale per la tab
+    //  _______________
+    // |0;0  c2  /     |h;0
+    // |       _/      |
+    // |     _/        |     Quadrato di contenimento della curva sx
+    // |    /          |
+    // |___/____c1_____|
+    // 0;h              h;h
+    QPainterPath tabPath;
+    const QPointF c1(0.63f, -0.04f), c2(0.52f, 0.97f);
+    const int h = selectedTabRect.height();
+    tabPath.moveTo(selectedTabRect.x(), selectedTabRect.y() + selectedTabRect.height() + 1); // QRect ha bordi di 1 px, compenso
+    tabPath.cubicTo(selectedTabRect.x() + h * c2.x(), selectedTabRect.y() + h * c2.y(),
+                 selectedTabRect.x() + h * c1.x(), selectedTabRect.y() + h * c1.y(),
+                 selectedTabRect.x() + h, selectedTabRect.y()); // Destination point
+
+    int dxStartBez = selectedTabRect.x() + selectedTabRect.width() - h - 2;
+    tabPath.lineTo(dxStartBez - 1, selectedTabRect.y()); // Larghezza della tab
+
+    // Disegna la bezièr dx
+    tabPath.cubicTo(dxStartBez + h * c1.x(), selectedTabRect.y() + h * c1.y(),
+                    dxStartBez + h * c2.x(), selectedTabRect.y() + h * c2.y(),
+                    selectedTabRect.x() + selectedTabRect.width() + 1, selectedTabRect.y() + selectedTabRect.height() + 1);
+
+
+
+    p.setPen(grayPen);
+    p.drawPath(tabPath);
+    tabPath.translate(0, -2);
+    p.setPen(blackPen);
+    p.drawPath(tabPath);
     // usa QPainterPath::intersects per mouse hit test
 }
