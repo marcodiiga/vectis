c9e9a58befc1cde5b3447541e6fef6052a2c0707
marco.diiga@gmail.com <marco.diiga@gmail.com>
2014-12-10 01:56:58 +0100

    fixed tab bezièr again, implemented tab system, many small uberprecise adjustments at pixel level, started implementing multitab and inactive tab system

--- vectis/UI/TabsBar/TabsBar.cpp
+++ vectis/UI/TabsBar/TabsBar.cpp
@@ -15,39 +15,26 @@ TabsBar::TabsBar( QWidget *parent )
     setAttribute( Qt::WA_OpaquePaintEvent, false );
     //qInstallMessageHandler(crashMessageOutput);
     setStyleSheet("QWidget { background-color: rgb(22,23,19); }");
+
+    //DEBUG
+    //DEBUG
+    // cazzeggia qui per provare le tab
+    m_tabs.push_back(std::make_unique<Tab>());
+    //DEBUG
 }
 
-void TabsBar::paintEvent ( QPaintEvent* ) {
-    QStyleOption opt; // Permette il setting del background via styleSheet
-    opt.init(this);
-    QPainter p(this);
-    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
-    p.setRenderHint(QPainter::Antialiasing);
-
-    // La barra orizzontale che si interrompe solo sulla tab selezionata corrente, è di due pixel
-    // e ha due colori:
-    //
-    //  -- nero rgb(11,11,10)   -- bcol1
-    //  -- grigio rgb(60,61,56) -- gcol2
-    const QColor outerBlackCol(11, 11, 10);
-    const QColor innerGrayCol(60, 61, 56);
-
-    // La tab attualmente selezionata (TODO: può essere nulla)
-    QRect selectedTabRect(5, 5, 150 /* Width */, rect().bottom() - 5);
-
-    // Disegna la barra orizzontale in basso che si collega al CodeTextEdit;
-    // questa corre per tutto il code editor *tranne* che per la selected tab
-    const QPen grayPen( innerGrayCol ), blackPen( outerBlackCol );
-    p.setPen(grayPen);
-    p.drawLine( rect().left(), rect().bottom(), selectedTabRect.left(), rect().bottom());
-    p.drawLine( selectedTabRect.right(), rect().bottom(), rect().right(), rect().bottom());
-    p.setPen(blackPen);
-    p.drawLine( rect().left(), rect().bottom() - 1, selectedTabRect.left(), rect().bottom() - 1);
-    p.drawLine( selectedTabRect.right(), rect().bottom() - 1, rect().right(), rect().bottom() - 1);
-
-    p.setPen(QColor(255,0,0)); // Debug
-    //p.drawRect(selectedTabRect); // Debug
-
+// Disegna una tab selezionata o meno dentro un rect
+QPainterPath TabsBar::drawTabInsideRect( QPainter& p, QRect& tabRect, bool selected,
+                                         const QPainterPath* sxTabRect, const QPainterPath* dxTabRect ) {
+    // Decide i colori per una tab selected o unselected
+    QColor topGradientColor, bottomGradientColor;
+    if( selected == false ) { // Unselected
+        topGradientColor = QColor(54, 54, 52);
+        bottomGradientColor = QColor(72, 72, 69);
+    } else {
+        topGradientColor = QColor(52, 53, 47);
+        bottomGradientColor = QColor(39, 40, 34);
+    }
     // Disegna due curve di bezièr e una linea orizzontale per la tab
     //  _______________
     // |0;0  c2  /     |h;0
@@ -57,38 +44,110 @@ void TabsBar::paintEvent ( QPaintEvent* ) {
     // |___/____c1_____|
     // 0;h              h;h
     QPainterPath tabPath;
-    const QPointF c1(0.52f, 0.97f), c2(0.63f, -0.04f);
-    const int h = selectedTabRect.height();
-    tabPath.moveTo(selectedTabRect.x(), selectedTabRect.y() + selectedTabRect.height() + 1); // QRect ha bordi di 1 px, compenso
-    qDebug() << selectedTabRect.x() << " " << selectedTabRect.y() + selectedTabRect.height() + 1;
-    tabPath.cubicTo(selectedTabRect.x() + h * c1.x(), selectedTabRect.y() + h * c1.y(),
-                 selectedTabRect.x() + h * c2.x(), selectedTabRect.y() + h * c2.y(),
-                 selectedTabRect.x() + h, selectedTabRect.y()); // Destination point
-    qDebug() << selectedTabRect.x() + h << " " << selectedTabRect.y();
+    p.setRenderHint(QPainter::Antialiasing);
+    const QPointF c1(0.6f, 1.0f-0.01f), c2(0.53f, 1.0f-1.0f);
+    const int h = tabRect.height();
+    tabPath.moveTo(tabRect.x(), tabRect.y() + tabRect.height() + 1); // QRect ha bordi di 1 px, compenso
+    //qDebug() << selectedTabRect.x() << " " << selectedTabRect.y() + selectedTabRect.height() + 1;
+    tabPath.cubicTo(tabRect.x() + h * c1.x(), tabRect.y() + h * c1.y(),
+                 tabRect.x() + h * c2.x(), tabRect.y() + h * c2.y(),
+                 tabRect.x() + h, tabRect.y()); // Destination point
+    //qDebug() << selectedTabRect.x() + h << " " << selectedTabRect.y();
 
-    int dxStartBez = selectedTabRect.x() + selectedTabRect.width() - h;
-    tabPath.lineTo(dxStartBez, selectedTabRect.y()); // Larghezza della tab
+    int dxStartBez = tabRect.x() + tabRect.width() - h;
+    tabPath.lineTo(dxStartBez, tabRect.y()); // Larghezza della tab
 
     // Disegna la bezièr dx
-    tabPath.cubicTo(dxStartBez + (h - h * c2.x()), selectedTabRect.y() + h * c2.y(),
-                    dxStartBez + (h - h * c1.x()), selectedTabRect.y() + h * c1.y(),
-                    selectedTabRect.x() + selectedTabRect.width(), selectedTabRect.y() + selectedTabRect.height() + 1);
+    tabPath.cubicTo(dxStartBez + (h - h * c2.x()), tabRect.y() + h * c2.y(),
+                    dxStartBez + (h - h * c1.x()), tabRect.y() + h * c1.y(),
+                    tabRect.x() + tabRect.width(), tabRect.y() + tabRect.height() + 1);
 
     // Riempie la tabPath con un gradiente di sfondo
-    QLinearGradient selectedTabGradient( selectedTabRect.topLeft(), selectedTabRect.bottomLeft() ); // Direzione verticale
-    selectedTabGradient.setColorAt( 1, QColor(39, 40, 34) ); // Colore finale è lo sfondo della code text box
-    selectedTabGradient.setColorAt( 0, QColor(52, 53, 47) ); // Colore iniziale (in alto) è più chiaro
-    QBrush selectedTabBrushFill( selectedTabGradient );
-    p.setBrush( selectedTabBrushFill );
-    p.fillPath( tabPath, selectedTabBrushFill );
+    QLinearGradient tabGradient( tabRect.topLeft(), tabRect.bottomLeft() ); // Direzione verticale
+    tabGradient.setColorAt( 1, bottomGradientColor ); // Colore finale è lo sfondo della code text box
+    tabGradient.setColorAt( 0, topGradientColor ); // Colore iniziale (in alto) è più chiaro
+    QBrush tabBrushFill( tabGradient );
+    p.setBrush( tabBrushFill );
+    p.fillPath( tabPath, tabBrushFill );
+
+    p.setRenderHint(QPainter::SmoothPixmapTransform);
 
     // Una volta riempito (fillato) lo sfondo, si può disegnare sopra un bordo grigio ed
-    // una path attaccata alla prima come un bordo nero traslata di 2 in alto
-    QPainterPath blackOuterTabPath = tabPath.translated(0, -2);
+    // una path attaccata alla prima come un bordo nero traslata di 1 in alto
+    const QPen grayPen( QColor(60, 61, 56) ), blackPen( QColor(11, 11, 10) ), intersectionPen( QColor(56,56,53) );
+    QPainterPath blackOuterTabPath = tabPath.translated(0, -1);
+    QPainterPath bottomLine; // Evita che il contorno abbia imperfezioni
+    bottomLine.moveTo(blackOuterTabPath.boundingRect().left(), blackOuterTabPath.boundingRect().bottom()-1);
+    blackOuterTabPath.lineTo(blackOuterTabPath.boundingRect().right(), blackOuterTabPath.boundingRect().bottom()-1);
+    blackOuterTabPath.subtracted(blackOuterTabPath);
     p.setPen(blackPen);
     p.drawPath(blackOuterTabPath);
+    if( sxTabRect != 0 ) {
+        QPainterPath sxRemnant = blackOuterTabPath.intersected(*sxTabRect);
+        p.setPen(intersectionPen);
+        p.drawPath(sxRemnant);
+    }
+    if( dxTabRect != 0 ) {
+        QPainterPath dxRemnant = blackOuterTabPath.intersected(*dxTabRect);
+        p.setPen(intersectionPen);
+        p.drawPath(dxRemnant);
+    }
     p.setPen(grayPen);
     p.drawPath(tabPath);
+
+    if(selected == false) { // Le tab unselected hanno una leggera ulteriore sfumatura in basso
+        p.setRenderHint(QPainter::Antialiasing, false);
+        p.setRenderHint(QPainter::HighQualityAntialiasing, false);
+        p.setPen(QPen(QColor(60,61,56)));
+        p.drawLine(tabRect.left() + tabRect.height() / 4, tabRect.bottom(),
+                   tabRect.right() - tabRect.height() / 4, tabRect.bottom());
+    }
+    return tabPath;
+}
+
+void TabsBar::paintEvent ( QPaintEvent* ) {
+    QStyleOption opt; // Permette il setting del background via styleSheet
+    opt.init(this);
+    QPainter p(this);
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+
+
+    // La barra orizzontale che si interrompe solo sulla tab selezionata corrente, è di due pixel
+    // e ha due colori:
+    //
+    //  -- nero rgb(11,11,10)   -- bcol1
+    //  -- grigio rgb(60,61,56) -- gcol2
+    const QColor outerBlackCol(11, 11, 10);
+    const QColor innerGrayCol(60, 61, 56);
+
+    p.setRenderHint(QPainter::Antialiasing, false);
+    p.setRenderHint(QPainter::HighQualityAntialiasing, false);
+
+    // Disegna la prima barra orizzontale in basso che si collega al CodeTextEdit;
+    // questa corre per tutto il code editor *tranne* che per la selected tab
+    const QPen grayPen( innerGrayCol ), blackPen( outerBlackCol );
+    p.setPen(blackPen);
+    p.drawLine( rect().left(), rect().bottom() - 1, rect().right(), rect().bottom() - 1);
+    // e disegna anche la seconda barra grigia orizzontale
+    p.setPen(grayPen);
+    p.drawLine(rect().left(), rect().bottom(), rect().right(), rect().bottom());
+
+    // TODO: DISEGNA QUI LE TAB -NON- SELEZIONATE, POI DISEGNA SOPRA LA LINEA ORIZZONTALE E INFINE QUELLA SELEZIONATA
+
+    // DEBUG - La tab attualmente selezionata (TODO: può essere nulla)
+    QRect unSelectedTabRect(5, 5, 150 /* Width */, rect().bottom() - 5);
+
+    QPainterPath tab = drawTabInsideRect(p, unSelectedTabRect, false);
+
+    QRect selectedTabRect(5+unSelectedTabRect.width() - 20, 5, 150 /* Width */, rect().bottom() - 5);
+
+    drawTabInsideRect(p, selectedTabRect, true, &tab, 0);
+
+    //p.setPen(QColor(255,0,0)); // Debug bordo tab
+    //p.drawRect(selectedTabRect); // Debug bordo tab
+
+    // Disegna la tab selezionata sopra a ogni altro controllo o bordo
+    //drawTabInsideRect(p, unSelectedTabRect, true);
 
     // TODO: usa QPainterPath::intersects per mouse hit test
 }
--- vectis/UI/TabsBar/TabsBar.h
+++ vectis/UI/TabsBar/TabsBar.h
@@ -2,21 +2,27 @@
 #define TABSBAR_H
 
 #include <QWidget>
+#include <memory>
 
 class Tab { // Questa classe rappresenta una tab del controllo
 public:
+    //TODO: il titolo
+    std::string m_title;
 };
 
 class TabsBar : public QWidget { // Questa classe rappresenta l'intero controllo
     Q_OBJECT
 public:
     explicit TabsBar( QWidget *parent = 0 );
+    QPainterPath drawTabInsideRect(QPainter& p, QRect& tabRect , bool selected ,
+                                   const QPainterPath* sxTabRect = 0, const QPainterPath* dxTabRect = 0);
 private:
 
     void paintEvent ( QPaintEvent* );
 
     QWidget *m_parent;
-    std::vector<Tab> m_tabs; // Il vettore delle tabs
+    std::list<std::unique_ptr<Tab>> m_tabs; // Il vettore delle tabs
+    size_t m_selectedTabIndex; // L'index della tab selezionata. -1 significa "nessuna"
 };
 
 #endif // TABSBAR_H
