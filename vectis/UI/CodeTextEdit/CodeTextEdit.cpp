4ac7877e02baac27863ffc61af5e0a630c58c612
Marco <marco.diiga@gmail.com>
2015-05-12 17:02:57 +0200

    Planning for the code edit control

--- vectis/UI/CodeTextEdit/CodeTextEdit.cpp
+++ vectis/UI/CodeTextEdit/CodeTextEdit.cpp
@@ -4,3 +4,34 @@ CodeTextEdit::CodeTextEdit(QWidget *parent) :
     QTextEdit(parent) {
 }
 
+
+/*
+
+Design for the code editor (main component for code editing)
+------------------------------------------------------------
+
+
+
+// We can't directly subclass a QPlainTextEdit, we need to render everything in the control.
+class Control : public QAbstractScrollArea {
+  paintEvent() {
+    QPainter painter(this->viewport());
+    
+    foreach visible line (only draw a subset of the file)
+      DrawLine()
+  }
+  DrawLine() {
+    // There will be multiple phases for drawing, e.g. background then edges, then text..
+    draw every word in the line according to its formatting (a lexer might read keywords and the
+      style might be set afterwards) with clipping (nowrap) or without clipping (wrap)
+  }
+};
+
+
+
+
+
+
+
+
+*/
